c553c75e44a900af6e3e1f830c48598c4c4fa965
prakash <prakashbaireddy@gmail.com>
2025-02-05 23:14:39 +0530

    vpc-module

--- terraform-module-vpc/main.tf
+++ terraform-module-vpc/main.tf
@@ -106,37 +106,43 @@ resource "aws_route_table" "database" {
  
 }
 
-
+# public route
 resource "aws_route" "public" {
   route_table_id            = aws_route_table.public.id
   destination_cidr_block    = "0.0.0.0/0"
   gateway_id = aws_internet_gateway.igw.id
 }
 
+# private route
 resource "aws_route" "private" {
   route_table_id            = aws_route_table.private.id
   destination_cidr_block    = "0.0.0.0/0"
   nat_gateway_id = aws_nat_gateway.example.id
 }
 
+# database route
 resource "aws_route" "database" {
   route_table_id            = aws_route_table.database.id
   destination_cidr_block    = "0.0.0.0/0"
   nat_gateway_id = aws_nat_gateway.example.id
 }
 
+
+# public subents association
 resource "aws_route_table_association" "public" {
   count = length(var.public_subnet_cidrs)
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
 
+# private subents association
 resource "aws_route_table_association" "private" {
   count = length(var.private_subnet_cidrs)
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.id
 }
 
+# database subents association
 resource "aws_route_table_association" "database" {
   count = length(var.database_subnet_cidrs)
   subnet_id      = aws_subnet.database[count.index].id
